6e5198d7f7fc8d827d467de1037e4d22c54b22c4
PedroSouza2302 <apolisouza1@gmail.com>
2026-04-20 11:08:14 -0300

    Projeto finalizado

---
+++ .vscode/assets/script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/Avatar-Light.png")
+  } else {
+    img.setAttribute("src", "./assets/Avatar.png")
+  }
+  if (html.classList.contains("light")) {
+    img.setAttribute("alt", "LOGO AMPERI ELETRONICS")
+  } else {
+    img.setAttribute("alt", "LOGO AMPERI ELETRONICS")
+  }
+}
